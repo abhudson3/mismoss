10650ce1cec6abbfa8f3124cb4d363dc3e626ba3
abhudson3 <112576341+abhudson3@users.noreply.github.com>
2023-12-27 22:20:32 -0600

    working without basefile

---
+++ mossUtil2.py
@@ -0,0 +1,99 @@
+import mosspy
+import subprocess
+import re
+
+def extract_substring_after_moss(input_string):
+    pattern = r"mis.*?submissions"
+    match = re.search(pattern, input_string)
+    if match:
+        return match.group()
+    else:
+        return "No match found for the pattern"
+
+
+def move_js_files_to_top(root_dir, language):
+    # Loop through all items in the root directory
+    for item in os.listdir(root_dir):
+        path = os.path.join(root_dir, item)
+        # Check if the item is a directory
+        if os.path.isdir(path):
+            # Walk through the directory
+            for subdir, dirs, files in os.walk(path):
+                for file in files:
+                    # Check if the file is a JavaScript file
+                    if file.endswith(language):
+                        source_file_path = os.path.join(subdir, file)
+                        destination_file_path = os.path.join(path, file)
+                        # Move the JavaScript file to the top of the directory
+                        shutil.move(source_file_path, destination_file_path)
+                        print(f"Moved: {source_file_path} to {destination_file_path}")
+
+
+
+
+def clone_repos(assnCode, directory):
+    """List repositories of a given GitHub assignment code."""
+    command = ["gh", "classroom", "clone", "student-repos", "-a", assnCode, "--directory", directory, "--all"]
+
+
+    # runs the above repo clone command, which returns the output of the command
+    unfilteredOutputDir = run_cli_command(command, "submission")
+    # filters the command output to get the name of the directory that the repos were cloned into
+    outputDir = extract_substring_after_moss(unfilteredOutputDir)
+    # returns the directory that the repos were cloned into
+    return outputDir
+
+def run_cli_command(command, keyword):
+    """Run a command and return the output."""
+    result = subprocess.run(command, capture_output=True, text=True)
+    print(result.stdout)
+    if result.returncode != 0:
+        raise Exception(f"Error running command: {result.stderr}")
+    captured_output = process_output(result.stdout, keyword)
+    return captured_output
+
+def process_output(output, keyword):
+    """Process the output to extract the desired information."""
+    # Placeholder for how you might process the output
+    # For example, capturing lines that contain a specific keyword
+    for line in output.split('\n'):
+        if keyword in line:
+            return line  # or accumulate in a list if multiple lines are expected
+    return "No relevant information found"  # Placeholder response
+
+
+
+def main():
+    try:
+        userid = 813921095
+        assnCode = input("\nMISMoss - 2023 \n \nWhat is the assignment code of the assignment you would like to check?\n")
+        directory = input("\nWhat is the directory you would like to run the program in? \nIf one does not exist by the name provided, one will be created.\n")
+        language = input("\nPlease type in the file extension for the language you would like to check in the following format: .js\n")
+        # basefile = input("\nWould you like to include a basefile?")
+        fullLanguage = ""
+        if(language == ".js"):
+            fullLanguage = "javascript"
+        elif(language == ".cs"):
+            fullLanguage = "csharp"
+        # output directory is equal to the name of the directory all of the repos were cloned into
+        outputDir = clone_repos(assnCode, directory)
+        # print(outputDir)
+        # outputDir = "mis321-pa2-submissions"
+        m = mosspy.Moss(userid, fullLanguage)
+
+        # Submission Files
+        m.setDirectoryMode(1)
+        m.addFilesByWildcard("./" + directory + "/" + outputDir + "/*/*" + language)
+
+  
+        url = m.send(lambda file_path, display_name: print('*', end='', flush=True))
+        print()
+
+        print ("Report Url: " + url)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+
+if __name__ == "__main__":
+    main()
